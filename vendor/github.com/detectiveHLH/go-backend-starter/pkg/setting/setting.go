7d142769c44a95bf9baa0c8b2bd34a41c9094de4
hulunhao <545759184@qq.com>
2018-09-04 15:31:19 +0800

    Add package management

---
+++ vendor/github.com/detectiveHLH/go-backend-starter/pkg/setting/setting.go
@@ -0,0 +1,41 @@
+package setting
+
+import (
+	"gopkg.in/ini.v1"
+	"log"
+)
+
+// 定义服务器配置结构体
+type Server struct {
+	Ip   string
+	Port string
+}
+type Database struct {
+	Type        string
+	User        string
+	Password    string
+	Host        string
+	Name        string
+	TablePrefix string
+}
+
+var ServerSetting = &Server{}
+var DatabaseSetting = &Database{}
+var config *ini.File
+
+func Setup() {
+	var err error
+	config, err = ini.Load("config/app.ini")
+	if err != nil {
+		log.Fatal("Fail to parse 'config/app.ini': %v", err)
+	}
+	mapTo("server", ServerSetting)
+	mapTo("database", DatabaseSetting)
+}
+
+func mapTo(section string, v interface{}) {
+	err := config.Section(section).MapTo(v)
+	if err != nil {
+		log.Fatalf("Cfg.MapTo RedisSetting err: %v", err)
+	}
+}
