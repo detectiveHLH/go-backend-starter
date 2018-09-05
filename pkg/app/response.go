8e4defab9840fa169a73aa60406aa98bc2b3a2df
hulunhao <545759184@qq.com>
2018-09-05 16:40:48 +0800

    add comment response

--- pkg/app/response.go
+++ pkg/app/response.go
@@ -9,6 +9,12 @@ type Gin struct {
 	C *gin.Context
 }
 
+/**
+统一后端返回格式
+@param	httpCode	http状态码
+@param	errCode		错误码
+@param	data		返回数据
+ */
 func (g *Gin) Response(httpCode, errCode int, data interface{}) {
 	g.C.JSON(httpCode, gin.H{
 		"code": httpCode,
